f4ee4f1800638dd24b559b3caf2988e20897cf57
AndriyKaluzhiny <88110861+AndriyKaluzhiny@users.noreply.github.com>
2022-10-29 15:56:31 +0300

    Delete UserDao.java

--- UserDao.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.lgs.dao;
-
-import ua.lviv.lgs.domain.User;
-import ua.lviv.lgs.shared.AbstractDao;
-
-public interface UserDao extends AbstractDao<User> {
-    User readByEmail(String email);
-}
